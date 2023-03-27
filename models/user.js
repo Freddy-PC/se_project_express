756e03fe1c2cf27caac65b713ca2672a2104c04d
Freddy PC <freddythepc@gmail.com>
2023-03-27 09:06:29 -0400

    Add default values and email + password field

--- models/user.js
+++ models/user.js
@@ -2,22 +2,45 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
-  },
-  avatar: {
-    type: String,
-    required: true,
-    validate: {
-      validator(v) {
-        return validator.isURL(v);
-      },
-      message: "You must enter a valid URL",
+    name: {
+        type: String,
+        default: "Elise Bouer",
+        required: true,
+        minlength: 2,
+        maxlength: 30,
     },
-  },
+    avatar: {
+        type: String,
+        default:
+            "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Elise.png",
+        required: true,
+        validate: {
+            validator(v) {
+                return validator.isURL(v);
+            },
+            message: "You must enter a valid URL",
+        },
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator(v) {
+                return validator.isURL(v);
+            },
+            message: "You must enter a valid email",
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: {
+            validator(v) {
+                return validator.isURL(v);
+            },
+            message: "You must enter a valid password",
+        },
+    },
 });
 
 module.exports = mongoose.model("users", userSchema);
