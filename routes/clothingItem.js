4b517043bdcdcf1579c649a05f1fac942a141ca4
Freddy PC <freddythepc@gmail.com>
2023-03-28 15:44:10 -0400

    Add authentification from middleware

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,23 +1,24 @@
 const router = require("express").Router(); // Import
 
 const {
-  createItem,
-  getItems,
-  deleteItems,
-  likeItem,
-  dislikeItem,
+    createItem,
+    getItems,
+    deleteItems,
+    likeItem,
+    dislikeItem,
 } = require("../controllers/clothingItem"); // Host
+const auth = require("../middlewares/auth");
 
 // Create
-router.post("/", createItem);
+router.post("/", auth, createItem);
 // Get
-router.get("/", getItems);
+router.get("/", auth, getItems);
 // Delete = '/items/:itemId'
-router.delete("/:itemId", deleteItems);
+router.delete("/:itemId", auth, deleteItems);
 
 // Like
-router.put("/:itemId/likes", likeItem);
+router.put("/:itemId/likes", auth, likeItem);
 // Dislike
-router.delete("/:itemId/likes", dislikeItem);
+router.delete("/:itemId/likes", auth, dislikeItem);
 
 module.exports = router;
