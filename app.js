7e8f8803f75baa0927fb9f615b1814eec15f55b4
Freddy PC <freddythepc@gmail.com>
2023-03-28 14:18:43 -0400

    Remove user routes & add routes into app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { login, createUser } = require("../controllers/user");
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -11,15 +12,17 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 const routes = require("./routes");
 
 app.use(express.json());
-app.use((req, res, next) => {
-  // user object added to each request
-  req.user = {
-    _id: "640b8ee18ce59b8d84d581f0",
-  };
-  next();
-});
+// app.use((req, res, next) => {
+//     // user object added to each request
+//     req.user = {
+//         _id: "640b8ee18ce59b8d84d581f0",
+//     };
+//     next();
+// });
 app.use(routes);
+app.post("/signin", login);
+app.post("/signup", createUser);
 
 app.listen(PORT, () => {
-  console.log(`App listening at port ${PORT}`);
+    console.log(`App listening at port ${PORT}`);
 });
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 
-const { createUser, getUser, getUsers } = require("../controllers/user");
+// const { createUser, getUser, getUsers } = require("../controllers/user");
+// // Create
+// router.post("/", createUser);
+// // Return one
+// router.get("/:userId", getUser);
 
-// Create
-router.post("/", createUser);
-// Return one
-router.get("/:userId", getUser);
-
-// Return all
-router.get("/", getUsers);
+// // Return all
+// router.get("/", getUsers);
 
 module.exports = router;
