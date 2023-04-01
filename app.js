04ec75320a21b79e64a5770cac6944b42d4e98f2
Freddy PC <freddythepc@gmail.com>
2023-04-01 12:21:11 -0400

    Fix syntax errors & add cors

--- app.js
+++ app.js
@@ -4,24 +4,19 @@ const { login, createUser } = require("./controllers/user");
 
 const { PORT = 3001 } = process.env;
 const app = express();
+const cors = require("cors"); // Connect front & back end
+app.use(cors());
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
-// Add options if mongoose is version 5 or error
 
 // Connect Routes
 const routes = require("./routes");
 
 app.use(express.json());
-// app.use((req, res, next) => {
-//     // user object added to each request
-//     req.user = {
-//         _id: "640b8ee18ce59b8d84d581f0",
-//     };
-//     next();
-// });
-app.use(routes);
 app.post("/signin", login);
 app.post("/signup", createUser);
+
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`App listening at port ${PORT}`);
--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const { JWT_SECRET } =
+const JWT_SECRET =
     "bbcc30a4def7083032b68d893cf6a55dd33239f9a8466989d7a52581c55b4ac7";
 
 module.exports = { JWT_SECRET };
