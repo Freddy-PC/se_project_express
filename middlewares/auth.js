7ea0f20a70d6f4f24909f33d7cb5172e7843597f
Freddy PC <freddythepc@gmail.com>
2023-04-01 11:57:12 -0400

    Add constant for error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,14 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
+const { ERROR_CODES } = require("../utils/errors");
 
 module.exports = (req, res, next) => {
     const { authorization } = req.headers;
 
     if (!authorization || !authorization.startsWith("Bearer ")) {
-        return res.status(401).send({ message: "Authorization Required" });
+        return res
+            .status(ERROR_CODES.Unauthorized)
+            .send({ message: "Authorization Required" });
     }
 
     // Verify token
@@ -15,7 +18,9 @@ module.exports = (req, res, next) => {
     try {
         payload = jwt.verify(token, JWT_SECRET);
     } catch (err) {
-        return res.status(401).send({ message: "Authorization Required" }); // unauhorized user tried to access
+        return res
+            .status(ERROR_CODES.Unauthorized)
+            .send({ message: "Authorization Required" }); // unauhorized user tried to access
     }
     // Payload recieved
 
