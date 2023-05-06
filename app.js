f67fcfb23ffdde815a941de8c586f961e4bbb3c8
Freddy PC <freddythepc@gmail.com>
2023-05-06 12:17:29 -0400

    Add winston & loggers

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors"); // Connect front & back end
 const { login, createUser } = require("./controllers/user");
 const ErrorHandler = require("./middlewares/ErrorHandler");
 const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -20,7 +21,9 @@ app.use(express.json());
 app.post("/signin", login);
 app.post("/signup", createUser);
 
+app.use(requestLogger);
 app.use(routes);
+app.use(errorLogger);
 app.use(errors());
 app.use(ErrorHandler);
 
